147812751939e62d16c335622d8e17116800bd42
jason olendorff <jasonolendorff@gmail.com>
2025-10-18 00:28:16 -0500

    Delete e-candle-vercel-ready directory

--- e-candle-vercel-ready/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true } // avoids remote loader setup for quick testing
-};
-module.exports = nextConfig;
